2f0316f03b2c1c850dece8953718fc7411a1ca08
alina <kostenkoalina97@icloud.com>
2025-10-20 13:58:23 +0200

    fix(imports): update svg imports & component usages

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,18 @@
+import { TouchableOpacity } from 'react-native'
+import tw from 'twrnc'
+import BackIcon from '/assets/svg/BackIcon.svg'
+import { RootStackParamList } from '@screens/types/root'
+import { StackNavigationProp } from '@react-navigation/stack'
+
+type BackButtonProps = {
+  navigation: StackNavigationProp<RootStackParamList>
+}
+
+export const BackButton = ({ navigation }: BackButtonProps) => (
+  <TouchableOpacity
+    onPress={() => navigation.goBack()}
+    style={tw`bg-white rounded-full p-4 absolute top-12 left-3 z-10`}
+  >
+    <BackIcon width={24} height={24} />
+  </TouchableOpacity>
+)
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,58 @@
 import { View, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
+import { RootStackParamList } from './types/root'
+import ActivityCard from '../components/ActivityCard'
+import tw from 'twrnc'
+import { useGetActivitiesQuery } from '../api/activities'
+import Loader from '../components/Loader'
+import Error from '../components/Error'
+import { Activity } from '../api/apiTypes'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { FlatList } from 'react-native-gesture-handler'
+
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>
+
+const keyExtractor = (activity: Activity) => activity.id.toString()
 
 const HomeScreen = () => {
+  const navigation = useNavigation<HomeScreenNavigationProp>()
+  const { data: activities, isLoading, isError } = useGetActivitiesQuery()
+
+  const handleActivityPress = (activity: Activity) => {
+    navigation.navigate('ActivityDetails', { activity })
+  }
+
+  if (isLoading) {
+    return <Loader />
+  }
+
+  if (isError) {
+    return <Error />
+  }
+
   return (
-    <View>
-      <Text>Hello, developer!</Text>
-    </View>
+    <SafeAreaView style={tw`flex-1 bg-white`} edges={['top']}>
+      <View style={tw`flex-1 p-4`}>
+        <Text
+          style={[
+            tw`text-base text-center mt-4 mb-9 text-black`,
+            { fontFamily: 'Abel' },
+          ]}
+        >
+          Activities
+        </Text>
+        <FlatList
+          data={activities}
+          keyExtractor={keyExtractor}
+          renderItem={({ item }) => (
+            <ActivityCard activity={item} onPress={handleActivityPress} />
+          )}
+          contentContainerStyle={{ gap: 10, paddingBottom: 20 }}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
