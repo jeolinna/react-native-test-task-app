445be874e15f027d09c80d16201b32d46fb9ad73
alina <kostenkoalina97@icloud.com>
2025-10-29 10:08:05 +0100

    fix(api): use square bracket notation for API_BASE_URL to satisfy TS

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Config from 'react-native-config'
 
 export const api = axios.create({
-  baseURL: Config['API_BASE_URL'],
+  baseURL: Config.API_BASE_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 })
